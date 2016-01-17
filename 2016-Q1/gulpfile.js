78bc1c56ef6708dc4752040b7de130e411e01d43
dacy <dacy.dai@trantect.com>
2016-01-17 22:17:54 +0800

    route is ok and upload is ok and add jade template

--- 2016-Q1/gulpfile.js
+++ 2016-Q1/gulpfile.js
@@ -1,56 +1,61 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var coffee = require("gulp-coffee");
-var server = require( 'gulp-develop-server' );
+var server = require( 'gulp-nodemon' );
 var browserSync = require('browser-sync').create();
 
+var BROWSER_RELOAD_DELAY = 2000;
+
+gulp.task("default",["browser:sync"],function(){
+	gulp.watch("./server/**/*.coffee",["compile:coffee"]);		
+});
+
+gulp.task("browser:sync",["server:start"],function(){
+	browserSync.init(null,{
+			proxy: "http://localhost:4000",
+			port: 7000
+	});
+});
+
+gulp.task("server:start",function(cb){
+	var run_flag = false;
+	return server({
+		script:"./deploy/server/index.js",
+		watch:["./deploy/server"],
+		ext: 'js'
+	}).on("start",function(){
+		if (!run_flag){
+			cb();
+		};
+		run_flag = true;
+	}).on("restart",function(){
+		console.log("restart");
+		setTimeout(function reload(){
+			console.log("reload");
+			browserSync.reload({
+				stream:true
+			});
+		},BROWSER_RELOAD_DELAY);
+	});
+});
+
+// compile typescript
 gulp.task("compile:ts",function(){
-	console.log("compiling typescript...");
 	return gulp.src(["./server/**/*.ts"])
 		.pipe(ts({module:"commonjs"}))
 		.js.pipe(gulp.dest("./deploy/server"));
 });
 
+// compile coffeescript
 gulp.task("compile:coffee",function(){
-	console.log("compiling coffeescript...");
 	return gulp.src(["./server/**/*.coffee"])
 		.pipe(coffee())
 		.pipe(gulp.dest("./deploy/server"))
+		.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task("reload:coffee",["compile:coffee"],function(){
-	server.restart(function(error){
-		console.log("call cb");
-		browserSync.reload();
-	});
+gulp.task("deploy:static",function(){
+	return gulp.src(["server/views/*"])
+				.pipe(gulp.dest("deploy/server/views"))
 });
 
-gulp.task("start:server",function(){
-	console.log("b");
-	server.listen({path:"./deploy/server/index.js"},function(error){
-		console.log("c");
-		browserSync.init({
-			proxy: "http://localhost:3000",
-			files: ["./deploy/server/**/*.*"],
-			port: 4000
-		});
-		console.log("browserSync",browserSync);
-	});
-});
-
-gulp.task("restart:server",function(){
-	server.restart();
-});
-
-gulp.task("reload",function(){
-	console.log("call reload");
-});
-
-gulp.task("watch",function(){
-	gulp.watch("./server/**/*.ts",["compile:ts","restart:server","reload"]);
-});
-
-gulp.task("default",["start:server"],function(){
-	console.log("TEST");
-	gulp.watch("./server/**/*.coffee",["reload:coffee"]);		
-});
--- 2016-Q1/server/index.coffee
+++ 2016-Q1/server/index.coffee
@@ -1,28 +1,56 @@
 koa = require "koa"
 fs = require "fs"
 serve = require "koa-static"
+router = require "koa-router"
+co = require "co"
+views = require "co-views"
+parse = require "co-busboy"
+path = require "path"
+os = require "os"
+Promise = require "bluebird"
+# fs.readdir = Promise.promisify fs.readdir
 
+route = router()
 app = koa()
+render = views(__dirname+"/views",map: { html: 'swig' });
 
-# app.use (next) ->
-# 	yield next
-# 	this.body = "3456 Hello dacy,I'm coffeescript with koa.js and watch..."
-app.use serve(__dirname)
+file_path = __dirname+"/upload"
 
+app.use serve(__dirname+"/views")
 
-app.use (next) ->
-  if 'POST' != this.method
-    return yield next;
-
-  parts = parse(this);
-  
+route.post "/upload",(next)->
+  parts = parse(this);  
   while part = yield parts
-    stream = fs.createWriteStream(path.join(os.tmpdir(), Math.random().toString()))
+    stream = fs.createWriteStream(path.join(file_path, part.filename))
     typeof(part)
     part.pipe(stream)
     console.log('uploading %s -> %s', part.filename, stream.path)
+    this.redirect('/file_list');  
 
-  this.redirect('/');
+route.get "/",(next)->
+  console.log "get / "
+  this.body = 'Hello World for test watch with new gulpfile,i hope!';
 
+route.get "/upload_file",(next)->
+  this.body = yield render("uploade_file.html");
+  
+route.get "/file_list",(next)->
+  fake1 = 
+    name:"a",
+    upload_time:1,
+    size:2,
+    md5:"3"
+  fake2 = 
+    name:"a",
+    upload_time:1,
+    size:2,
+    md5:"3"  
+  console.log "call here"
+  fs.readdir file_path,(error,files)=>
+    console.log "files ",files
+    this.body = yield render("file_list.jade",{file_list:[fake1,fake2]});
 
-app.listen 3000
+app.use (route.routes())
+app.use (route.allowedMethods())
+
+app.listen 4000
